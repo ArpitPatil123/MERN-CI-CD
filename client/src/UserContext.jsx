bc6672b05358793c77d54d805d1e0b9648320e5c
arpit patil <arpitpatilap122833@gmail.com>
2024-08-18 23:01:36 +0530

    update

---
+++ client/src/UserContext.jsx
@@ -0,0 +1,24 @@
+import {createContext, useEffect, useState} from "react";
+import axios from "axios";
+
+export const UserContext = createContext({});
+
+export function UserContextProvider({children}) {
+  const [user,setUser] = useState(null);
+  const [ready,setReady] = useState(false);
+
+  const data = {user,setUser,ready}
+  useEffect(() => {
+    if (!user) {
+      axios.get('/profile').then(({data}) => {
+        setUser(data);
+        setReady(true);
+      });
+    }
+  }, []);
+  return (
+    <UserContext.Provider value={data}>
+      {children}
+    </UserContext.Provider>
+  );
+}
